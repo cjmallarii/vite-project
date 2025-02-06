5a689d42cb8fcbde9fe125adfedaad19972f5987
cjmallarii <carlmallari14@gmail.com>
2025-02-06 21:53:05 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Carl Junnel Mallari</h1>
+      <div className="box">CPEITEL</div>
+    </div>
+  );
+};
+
+export default App;
